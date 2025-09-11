7763b861c02175169abc1bff5a02fef0e34a447c
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-09-11 18:53:51 +0900

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['https://localhost:5173'], // 허용할 출처
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
+    credentials: true, // 쿠키를 포함한 요청을 허용할지 여부
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
