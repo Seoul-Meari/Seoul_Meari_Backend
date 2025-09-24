af287c0b7f555461a1797bab12455e6abc639d66
mogiyoon <146794720+mogiyoon@users.noreply.github.com>
2025-09-25 01:29:31 +0900

    fix: hot fix ip

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://localhost:5173'], // 허용할 출처
+    origin: [
+      'http://localhost:5173',
+      'https://localhost:5173',
+      'https://dev.d1kkujxdgesuxm.amplifyapp.com',
+    ], // 허용할 출처
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // 허용할 HTTP 메서드
     allowedHeaders: ['Content-Type', 'Authorization'], // 허용할 헤더 명시
     credentials: true, // 쿠키를 포함한 요청을 허용할지 여부
